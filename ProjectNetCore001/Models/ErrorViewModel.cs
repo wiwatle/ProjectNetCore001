c88a2554f7d1351fca93988df0d7dcc1d1a9eebf
wiwatle <tonn_lephon1@hotmail.com>
2026-02-21 18:38:35 +0700

    Add project files.

---
+++ ProjectNetCore001/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjectNetCore001.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
